92b2f3a36233e21f7ab1d32bf67011f67f2c912a
mattiavisona <mattiav2706@gmail.com>
2023-05-23 23:59:58 +0200

    update

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,92 @@
+<footer>
+    <div class="container-1000">
+
+        <div class="footer-top">
+            <div class="footer-top-inner" class="container">
+
+                <div class="footer-left col-6">
+
+
+                    <ul class="footer-links">
+                        <li>
+                            <strong>DC Comics</strong>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                        </li>
+          
+                        <li>
+                            <strong>Shop</strong>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </li>
+          
+                        <li>
+                            <strong>DC</strong>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy Policy</li>
+                                <li>Terms Of Use</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <strong>Sites</strong>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </div>
+          
+                <div class="footer-right col-6">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="Logo">
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    <div class="footer-bottom-background">
+
+        <div class="container-1000">
+    
+            <div class="footer-bottom">
+                <div class="footer-bottom-left">
+                    <button type="button" class="btn bg-transparent btn-outline-primary rounded-0">Sign-up Now!</button>
+                </div>
+
+                <div class="footer-bottom-right">
+                    Follow Us
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook icona"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Facebook icona"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="Facebook icona"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Facebook icona"></a>
+                    <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Facebook icona"></a>
+                </div>
+              
+            </div>
+        </div>
+
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+    <div class="nav-more-info">
+        <div class="container-1000">
+            <ul>
+                <li>Dc power visa</li>
+                <li>Additional dc sites</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container-1000">
+
+        <nav class="nav-center">
+            <div class="nav-logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+            </div>
+
+            <div class="link-navbar d-flex gap-3">
+                <a href="{{route('home')}}">Home</a>
+                <a href="{{route('comics.index')}}">Comics</a>
+                <a href="#">Movie</a>
+                <a href="#">Tv</a>
+                <a href="#">Games</a>
+                <a href="#">Collectibles</a>
+                <a href="#">Video</a>
+                <a href="#">Fans</a>
+                <a href="#">News</a>
+                <a href="#">Shop</a>
+            </div>
+           
+
+
+            <div class="input">
+                <input type="text" class="form-control no-border" placeholder="Search">
+            </div>
+        </nav>
+
+    </div>
+
+    <div class="img-header-container">
+
+    </div>
+</header>
